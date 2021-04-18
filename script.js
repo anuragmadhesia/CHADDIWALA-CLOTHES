422bab9cbdfbcbeb28ae2991bb8791b3fd1364d5
anuragmadhesia <madhesiaanurag99@gmail.com>
2021-04-18 18:42:27 +0530

    design completed

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     $(window).scroll(function(){
       if(this.scrollY > 105){
         $(".navbar").addClass("sticky");
+        $(".onscrolll").addClass("offscrolll");
         $(".goTop").fadeIn();
       }
       else{
         $(".navbar").removeClass("sticky");
+        $(".onscrolll").removeClass("offscrolll");
         $(".goTop").fadeOut();
       }
     });
